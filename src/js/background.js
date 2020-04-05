5df6b2d72b4ef0c8d7dc942c6d1e83b48cb63b1c
Carlos Catly <ccatly@gmail.com>
2020-04-04 23:32:43 -0700

    finds the text option and counts it in background js

--- src/js/background.js
+++ src/js/background.js
@@ -1,33 +1,8 @@
-/* background.js
- *
- * This file has an example of how to make variables accessible to other scripts of the extension.
- *
- * It also shows how to handle short lived messages from other scripts, in this case, from in-content.js
- *
- * Note that not all extensions need of a background.js file, but extensions that need to persist data after a popup has closed may need of it.
- */
+window.test = {}
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    window.test[request.url] = request.count
+})
 
-// A sample object that will be exposed further down and used on popup.js
-const sampleBackgroundGlobal = {
-    message: 'This object comes from background.js'
-};
-
-// Listen to short lived messages from in-content.js
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    // Perform any ther actions depending on the message
-    console.log('background.js - received message from in-content.js:', message);
-    // Respond message
-
-    console.log(message)
-    if (message.action == "start") {
-        console.log("got here")
-    }
-    else if (message.action == "stop") {
-
-    }
-
-    sendResponse('👍');
-});
-
-// Make variables accessible from chrome.extension.getBackgroundPage()
-window.sampleBackgroundGlobal = sampleBackgroundGlobal;
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.create({ url: 'popup.html' })
+})
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,48 +1,11 @@
-let btnStart = document.getElementById('start-button');
-let btnStop = document.getElementById('stop-button');
-let lblStatus = document.getElementById('status-label');
 
-let port = null;
-const sendPortMessage = message => port.postMessage(message);
-const messageHandler = message => {
-    console.log('popup.js - received message:', message);
-};
+document.addEventListener('DOMContentLoaded', function () {
 
-const getTab = () =>
-    new Promise(resolve => {
-        chrome.tabs.query(
-            {
-                active: true,
-                currentWindow: true
-            },
-            tabs => resolve(tabs[0])
-        );
-    });
+    const bg = chrome.extension.getBackgroundPage()
+    Object.keys(bg.test).forEach(function (url) {
+        const div = document.createElement('div')
+        div.textContent = `${url}: ${bg.test[url]}`
+        document.body.appendChild(div)
+    })
 
-btnStart.onclick = (element) => {
-    lblStatus.innerHTML = "Started";
-
-
-    // Handle port messages
-    
-
-    getTab().then(tab => {
-        // Connects to tab port to enable communication with inContent.js
-        port = chrome.tabs.connect(tab.id, { name: 'chrome-extension-template' });
-        // Set up the message listener
-        port.onMessage.addListener(messageHandler);
-        // Send a test message to in-content.js
-        sendPortMessage('after pressing start');
-        chrome.runtime.sendMessage({ action: "start" }, (response) => { });
-    });
-    
-}
-
-btnStop.onclick = (element) => {
-    lblStatus.innerHTML = "Stopped";
-    console.log("stopped")
-    chrome.runtime.sendMessage({ action: "stop" }, (response) => { });
-}
-
-
-document.addEventListener('DOMContentLoaded', initPopupScript);
+}, false)
