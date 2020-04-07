992459a10725281e654f1c0c60f623fa6d9550c4
Carlos Catly <ccatly@gmail.com>
2020-04-06 22:20:42 -0700

    added basic buttons

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,8 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    let btnStart = document.getElementById('start-button');
+    let btnStop = document.getElementById('stop-button');
+    let lblStatus = document.getElementById('status-label');
+
+    btnStart.onclick = (element) => {
+        lblStatus.innerHTML = "Started";
+    }
+
+    btnStop.onclick = (element) => {
+        lblStatus.innerHTML = "Stopped";
+    }
+
     const bg = chrome.extension.getBackgroundPage()
-    Object.keys(bg.test).forEach(function (url) {
+    Object.keys(bg.test).forEach((url) => {
         const div = document.createElement('div')
         div.textContent = `${url}: ${bg.test[url]}`
         document.body.appendChild(div)
