8a8ca3da8e4191e706acf8d8a64802872c5c8fde
Carlos Catly <ccatly@gmail.com>
2020-04-12 21:46:06 -0700

    added a state boolean

--- src/js/background.js
+++ src/js/background.js
@@ -1,8 +1,11 @@
 window.test = {}
+let running = false;
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     window.test[request.url] = request.firstLink.innerHTML;
 
     if (request.action == "start") {
+        running = true;
         startAutomation(request);
     }
     else if (request.action == "stop") {
