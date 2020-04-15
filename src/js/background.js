7b3a0abcf43aef973659b44f3e79dacd9039c3d9
Carlos Catly <ccatly@gmail.com>
2020-04-14 22:45:53 -0700

    added click action based on position

--- src/js/background.js
+++ src/js/background.js
@@ -2,7 +2,7 @@ window.test = {}
 let running = false;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    window.test[request.url] = request.firstLink.innerHTML;
+    window.test[request.url] = request.firstLink;
 
     if (request.action == "start") {
         running = true;
@@ -18,7 +18,7 @@ chrome.browserAction.onClicked.addListener((tab) => {
 })
 
 const startAutomation = (request)=>{
-    if (request.firstLink) request.firstLink.click();
+    if (request.firstLink) document.elementFromPoint(request.firstLink.x, request.firstLink.y);
     if (window.test.start) {
         window.test.start += 1;
     } else {
